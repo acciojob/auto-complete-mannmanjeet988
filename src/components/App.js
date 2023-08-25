20c774d9a97d9242ddf525aa37743081dc2f8dd5
mannmanjeet988 <mannmanjeet988@gmail.com>
2023-08-25 19:56:33 +0530

    Done auto complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+  const [filteredFruits, setFilteredFruits] = useState(fruits);
+
+  const [searchTerm, setSerachTerm] = useState("");
+
+  useEffect(() => {
+    filterFruits();
+  }, [searchTerm]);
+
+  const filterFruits = () => {
+    const filteredFruitsArr = fruits.filter((fruit) =>
+      fruit.toLowerCase().startsWith(searchTerm.toLowerCase())
+    );
+    setFilteredFruits(filteredFruitsArr);
+    console.log(filteredFruits);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input
+        type="text"
+        onChange={(e) => setSerachTerm(e.target.value)}
+        value={searchTerm}
+      />
+      {filteredFruits && (
+        <ul>
+          {
+          filteredFruits.map((item) => (
+            <li>{item}</li>
+          ))
+          }
+        </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
